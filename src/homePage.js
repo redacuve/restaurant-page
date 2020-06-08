3228cb9c791a0b6daa7a93291ebffa8d49d26bbf
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-08 17:07:42 -0500

    finished homepage

--- src/homePage.js
+++ src/homePage.js
@@ -2,9 +2,14 @@ import Banner from './img/home_banner.png';
 
 export default function homePage() {
   const homepage = document.createElement('div');
-  homepage.innerHTML = `<div class="">
+  homepage.innerHTML = `<div>
                           <img src="${Banner}" alt="banner" width="100%">
                         </div>
-                        <p>HomePage</p>`;
+                        <div class="bg-orange-600 text-white text-6xl font-extrabold flex flex-wrap justify-center">
+                          <p class="mr-10">Tasty!</p>
+                          <p class="mr-10">Ready and Hot!</p>
+                          <p class="mr-10">Just for You!</p>
+                          <p class="mr-10">Enjoy!</p>
+                        </div>`;
   return homepage;
 }
