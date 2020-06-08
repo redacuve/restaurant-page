7abd764fec993f9739ee0d7453c2c4ab346c9eb6
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-08 15:42:28 -0500

    added elements on homepage

--- src/homePage.js
+++ src/homePage.js
@@ -2,6 +2,9 @@ import Banner from './img/home_banner.png';
 
 export default function homePage() {
   const homepage = document.createElement('div');
-  homepage.innerHTML = `<p>HomePage</p>`;
+  homepage.innerHTML = `<div class="">
+                          <img src="${Banner}" alt="banner" width="100%">
+                        </div>
+                        <p>HomePage</p>`;
   return homepage;
 }
