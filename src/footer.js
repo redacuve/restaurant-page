8cfe4bcce5e7bebe0e5b0d9118e52e87df5eed25
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-08 16:50:08 -0500

    finished footer

--- src/footer.js
+++ src/footer.js
@@ -4,19 +4,20 @@ import '@fortawesome/fontawesome-free/js/brands'
 
 export default function footer(){
     const footer = document.createElement('footer');
-    
+    footer.classList.add('flex', 'text-center', 'mb-4', 'flex-wrap', 'border-solid', 'border-t-4');
     footer.innerHTML = `
-        <div>
-            <p>Pizza Plaza </p>
+        <div class="w-full">
+            <p class="text-2xl">Pizza Plaza</p>
         </div>
-        <div>
-            Social Media:
-            <li><i class="fas fa-map-marked-alt"></i></li>
-            <li><i class="fab fa-tripadvisor"></i></li>
-            <li><i class="fab fa-twitter"></i></li>
-            <li><i class="fab fa-facebook"></i></li>
+        <div class="w-full">
+            <ul class="flex justify-center">
+                <li class="mr-3"><i class="fab fa-2x fa-facebook"></i></li>
+                <li class="mr-3"><i class="fab fa-2x fa-twitter"></i></li>
+                <li class="mr-3"><i class="fab fa-2x fa-tripadvisor"></i></li>
+                <li class="mr-3"><i class="fas fa-2x fa-map-marked-alt"></i></li>
+            </ul>
         </div>
-        <div>
+        <div class="w-full">
             <p>&copy; Pizza Plaza  by <em>Redacuve | 2020</em></p>
         </div>
     `;
