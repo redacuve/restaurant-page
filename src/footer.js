e63ba8c493030982778ead47b681e664f1a043f5
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-08 16:28:57 -0500

    added font-awesome

--- src/footer.js
+++ src/footer.js
@@ -1,14 +1,20 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/brands'
+
 export default function footer(){
     const footer = document.createElement('footer');
+    
     footer.innerHTML = `
         <div>
             <p>Pizza Plaza </p>
         </div>
         <div>
             Social Media:
-            <li><i></i></li>
-            <li><i></i></li>
-            <li><i></i></li>
+            <li><i class="fas fa-map-marked-alt"></i></li>
+            <li><i class="fab fa-tripadvisor"></i></li>
+            <li><i class="fab fa-twitter"></i></li>
+            <li><i class="fab fa-facebook"></i></li>
         </div>
         <div>
             <p>&copy; Pizza Plaza  by <em>Redacuve | 2020</em></p>
